a368efa65e4dc4f3f1982d84afc30a787a6fda5f
natthpatphummek <natthpatphummek@gmail.com>
2026-05-05 13:34:30 +0700

    first commit

---
+++ app/layouts/default.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import App from '~/app.vue';
+
+
+</script>
+
+
+<template>
+    <div>
+        <AppNavbar />
+        <slot />
+        <AppFooter />
+    </div>
+</template>
